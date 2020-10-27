fe35ac762352eaae0df130719fb10dfbe9622f19
bill.adams <bill.adams@philips.com>
2020-10-27 11:00:25 -0400

    added routes and hash strat routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'details', component: DetailsComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '',  redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
